a7b427658ed7a63cd2986d38079e4396eaa9c275
Mohit <Mohit.Sarraf@qualitestgroup.com>
2024-05-27 21:08:21 +0530

    LoginProject

---
+++ LoginPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace LoginProject
+{
+    class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver browser)
+        {
+            driver = browser;
+        }
+
+        private IWebElement AccountLogin => driver.FindElement(By.XPath("//a[@id=\"nav-link-accountList\"]"));
+
+        private IWebElement UserName => driver.FindElement(By.XPath("//input[@id=\"ap_email\"]")); 
+
+        private IWebElement BtnContinue => driver.FindElement(By.XPath("//input[@id=\"continue\"]"));
+
+        private IWebElement Password => driver.FindElement(By.XPath("//input[@id=\"ap_password\"]"));
+
+        private IWebElement BtnSignIn => driver.FindElement(By.XPath("//input[@id=\"signInSubmit\"]"));
+
+        public void GoToLogin()
+        {
+            AccountLogin.Click();
+        }
+
+        public void LoginApplication(string username, string password) 
+        {
+            UserName.SendKeys(username);
+            BtnContinue.Click();
+            Password.SendKeys(password);
+            BtnSignIn.Click();
+        }
+    }
+}
---
+++ LoginTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace LoginProject
+{
+    class LoginTest
+    {
+        private IWebDriver driver;
+        private LoginPage login;
+        string xmlFilePath = "C:\\Users\\mohit.sarraf\\source\\repos\\LoginProject\\LoginProject\\TestData.xml";
+
+        // Create an instance of XmlDocument
+        XmlDocument xmlDocument = new XmlDocument();
+
+        public static void Main()
+        {
+            LoginTest loginTest = new LoginTest();
+            loginTest.Setup();
+            loginTest.Login();
+        }
+
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+
+            // Load the XML document
+            xmlDocument.Load(xmlFilePath);
+
+            // Extract the XML document
+            XmlNode urlNode = xmlDocument.SelectSingleNode("/TestData/Site");
+            string url = urlNode.SelectSingleNode("Url")?.InnerText;
+
+            driver.Navigate().GoToUrl(url);
+            login = new LoginPage(driver);
+        }
+
+        public void Login()
+        {
+            login.GoToLogin();
+
+            // Load the XML document
+            xmlDocument.Load(xmlFilePath);
+
+            // Extract the XML document
+            XmlNode userNode = xmlDocument.SelectSingleNode("/TestData/User");
+            string username = userNode.SelectSingleNode("Username")?.InnerText;
+            string password = userNode.SelectSingleNode("Password")?.InnerText;
+
+            login.LoginApplication(username, password);
+        }
+    }
+}
